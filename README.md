7c1747965b240d3285ee474436e3a2e11c0db917
Devansh Sharma <devansh.sharma@gmail.com>
2025-12-25 00:44:26 +0530

    Implement init and new commands

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # Trasker
 
 Simple project level file based task tracker.
+
+## Directory Structure
+
+```
+.tasks
+├── 20251224-121342
+│   └── TASK.md
+│   └── [other files...]
+└── 20251224-174459
+    └── TASK.md
+```
+
+Each task is tracked as a directory, named with the task creation timestamp, containing a TASK.md file and other optional reference files.
+
+### TASK.md File Structure
+
+```markdown
+# Task Name
+
+- CATEGORY: TODO|FIX|PERF|SPIKE
+- STATUS: ACTIVE|COMPLETED|DROPPED
+
+Task Description
+```
+
+This file **must** follow the below format:
+- First line: Task name in Heading 1 style
+- Second line: Empty
+- Third line: Category of the task in bullet points style; should be one of the ones listed
+- Fourth line: Status of the task in bullet points style; should be one of the ones listed
+- Fifth line: Empty
+- Sixth line and onwards: Task description
--- main.go
+++ main.go
@@ -2,10 +2,41 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
+	"time"
 )
+
+const TASKS_DIR = ".tasks"
+const DATE_FMT = "20060102-150405"
+const TASK_MD_FILE = "TASK.md"
+const DEFAULT_TASK_CONTENTS = `# New Task
+
+- CATEGORY: TODO|FIX|PERF|SPIKE
+- STATUS: ACTIVE|COMPLETED|DROPPED
+
+This is a new task.`
+
+func dirExists(dirName string) bool {
+	info, err := os.Stat(dirName)
+	if errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+
+	return info.IsDir()
+}
+
+func isInitialized() bool {
+	if dirExists(".tasks") {
+		return true
+	}
+
+	fmt.Println("`.tasks` directory not found. Run `init` to setup Trasker in this project.")
+	return false
+}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
@@ -24,10 +55,58 @@ func main() {
 			cmd := parts[0]
 
 			switch cmd {
+			case "init":
+				if dirExists(TASKS_DIR) {
+					fmt.Println("`.tasks` directory already exists. Skipping initialization.")
+				} else {
+					err := os.Mkdir(TASKS_DIR, 0755)
+					if err != nil {
+						panic(err)
+					}
+					fmt.Println("Trasker initialized.")
+				}
+			case "new":
+				if isInitialized() {
+					now := time.Now()
+					for {
+						taskName := now.Format(DATE_FMT)
+						if !dirExists(taskName) {
+							dirName := fmt.Sprintf("%s/%s", TASKS_DIR, taskName)
+							fileName := fmt.Sprintf("%s/%s/%s", TASKS_DIR, taskName, TASK_MD_FILE)
+
+							os.Mkdir(dirName, 0755)
+							os.WriteFile(fileName, []byte(DEFAULT_TASK_CONTENTS), 0644)
+
+							editor := "vim"
+							for _, str := range os.Environ() {
+								if pair := strings.Split(str, "="); pair[0] == "EDITOR" {
+									editor = pair[1]
+									break
+								}
+							}
+
+							cmd := exec.Command(editor, fileName)
+							cmd.Stdin = os.Stdin
+							cmd.Stdout = os.Stdout
+							cmd.Stderr = os.Stderr
+
+							err := cmd.Run()
+							if err != nil {
+								panic(err)
+							}
+
+							fmt.Printf("Task `%s` created successfully.\n", taskName)
+							break
+						}
+						now = now.Add(time.Duration(1 * time.Second))
+					}
+				}
 			case "cls":
 				fmt.Print("\033[2J\033[H")
 			case "help":
 				fmt.Println("COMMANDS")
+				fmt.Println("  init\t\tinitialize Trasker in current directory")
+				fmt.Println("  new\t\tcreate and edit a new task")
 				fmt.Println("  cls\t\tclear the screen")
 				fmt.Println("  help\t\tdisplay this help")
 				fmt.Println("  exit\t\texit the program")
